877adbc734207ede475192ee05a45755a600277f
Steven Israel <malenchite@gmail.com>
2021-03-01 17:44:29 -0500

    Converted 'devour' buttons to form submits

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   /* TODO: Pull all burgers from DB and send to index */
-  res.render("index");
+  res.render("index", { burgers: [{ id: 1, burger_name: "Plain Ol' Burger", devoured: false }] });
 });
 
 router.post("/", (req, res) => {

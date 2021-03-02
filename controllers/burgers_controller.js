982c0ebfe27a8d4ff6760360e1cffcc9b2d85533
Steven Israel <malenchite@gmail.com>
2021-03-01 21:22:28 -0500

    Implemented routing behavior using burger model

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,22 +1,37 @@
 const express = require("express");
+const burger = require("../models/burger");
 
 const router = express.Router();
 
+/* GET to return index with all burger records */
 router.get("/", (req, res) => {
-  /* TODO: Pull all burgers from DB and send to index */
-  res.render("index", { burgers: [{ id: 1, burger_name: "Plain Ol' Burger", devoured: false }] });
+  /* Pull all burgers from DB and send to index */
+  burger.all(results =>
+    res.render("index", { burgers: results })
+  );
 });
 
+/* POST to add a new burger */
 router.post("/api/add", (req, res) => {
-  console.log(req.body);
-  /* TODO: Parse POST data and insert new burger */
-  res.redirect("/");
+  /* Create new burger with provided name */
+  burger.create({ burger_name: req.body.burger_name }, results => {
+    if (results.affectedRows === 0) {
+      return res.status(500).end();
+    }
+    res.redirect("/");
+  });
+
 });
 
+/* POST to update an existing burger by setting devoured to TRUE */
 router.post("/api/devour/:id", (req, res) => {
-  console.log(req.params.id);
-  /* TODO: Parse POST data and update burger as devoured */
-  res.redirect("/");
+  /* Update burger as "Devoured" */
+  burger.update({ devoured: true }, req.params.id, results => {
+    if (results.affectedRows === 0) {
+      return res.status(404).end();
+    }
+    res.redirect("/");
+  })
 });
 
 module.exports = router;
